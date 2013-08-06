f8f2d0219becd377b3d20b49dc8bf8b09c939b83
Masafumi Yokoyama <myokoym@gmail.com>
2013-08-06 21:35:09 +0900

    Add an assertion for playerOperate()

--- test/test-tuishogi.cpp
+++ test/test-tuishogi.cpp
@@ -64,15 +64,38 @@ test_computerOperate(void)
 void
 test_playerOperate_valid(void)
 {
+  // Arrange
   using namespace osl;
-  // TODO cleanup
   std::stringbuf  string_out;
   std::streambuf* std_out = std::cout.rdbuf(&string_out);
   NumEffectState state((SimpleState(HIRATE)));
+  const char* expected = "\
+P1-KY-KE-GI-KI-OU-KI-GI-KE-KY\n\
+P2 * -HI *  *  *  *  * -KA * \n\
+P3-FU-FU-FU-FU-FU-FU-FU-FU-FU\n\
+P4 *  *  *  *  *  *  *  *  * \n\
+P5 *  *  *  *  *  *  *  *  * \n\
+P6 *  * +FU *  *  *  *  *  * \n\
+P7+FU+FU * +FU+FU+FU+FU+FU+FU\n\
+P8 * +KA *  *  *  *  * +HI * \n\
+P9+KY+KE+GI+KI+OU+KI+GI+KE+KY\n\
+-\n\
+\n\
++7776FU\n";
+
+  // Act
   bool failed = playerOperate(state, "+7776FU");
+
   std::cout << std::flush;
   std::cout.rdbuf(std_out);
+
+  // TODO assert it as a std::string
+  std::string str = string_out.str();
+  const char* actual = str.c_str();
+
+  // Assert
   cut_assert_false(failed);
+  cut_assert_equal_string(expected, actual);
 }
 
 void
