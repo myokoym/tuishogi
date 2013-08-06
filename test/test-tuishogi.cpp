b1eb537b29bbbcd0463c91ad0a4a9ac121a4871e
Masafumi Yokoyama <myokoym@gmail.com>
2013-08-06 21:52:00 +0900

    Fix indent

--- test/test-tuishogi.cpp
+++ test/test-tuishogi.cpp
@@ -3,15 +3,15 @@
 
 namespace tuishogi {
 
-void
-test_showState(void)
-{
-  // Arrange
-  using namespace osl;
-  std::stringbuf  string_out;
-  std::streambuf* std_out = std::cout.rdbuf(&string_out);
-  NumEffectState state((SimpleState(HIRATE)));
-  const char* expected = "\
+  void
+  test_showState(void)
+  {
+    // Arrange
+    using namespace osl;
+    std::stringbuf  string_out;
+    std::streambuf* std_out = std::cout.rdbuf(&string_out);
+    NumEffectState state((SimpleState(HIRATE)));
+    const char* expected = "\
 P1-KY-KE-GI-KI-OU-KI-GI-KE-KY\n\
 P2 * -HI *  *  *  *  * -KA * \n\
 P3-FU-FU-FU-FU-FU-FU-FU-FU-FU\n\
@@ -24,52 +24,52 @@ P9+KY+KE+GI+KI+OU+KI+GI+KE+KY\n\
 +\n\
 \n";
 
-  // Act
-  showState(state);
+    // Act
+    showState(state);
 
-  std::cout << std::flush;
-  std::cout.rdbuf(std_out);
+    std::cout << std::flush;
+    std::cout.rdbuf(std_out);
 
-  // TODO assert it as a std::string
-  std::string str = string_out.str();
-  const char* actual = str.c_str();
+    // TODO assert it as a std::string
+    std::string str = string_out.str();
+    const char* actual = str.c_str();
 
-  // Assert
-  cut_assert_equal_string(expected, actual);
-}
+    // Assert
+    cut_assert_equal_string(expected, actual);
+  }
 
-void
-test_isMated(void)
-{
-  using namespace osl;
-  NumEffectState state((SimpleState(HIRATE)));
-  bool mated = isMated(state);
-  cut_assert_false(mated);
-}
+  void
+  test_isMated(void)
+  {
+    using namespace osl;
+    NumEffectState state((SimpleState(HIRATE)));
+    bool mated = isMated(state);
+    cut_assert_false(mated);
+  }
 
-void
-test_computerOperate(void)
-{
-  using namespace osl;
-  // TODO cleanup
-  std::stringbuf  string_out;
-  std::streambuf* std_out = std::cout.rdbuf(&string_out);
-  NumEffectState state((SimpleState(HIRATE)));
-  bool ended = computerOperate(state);
-  std::cout << std::flush;
-  std::cout.rdbuf(std_out);
-  cut_assert_false(ended);
-}
+  void
+  test_computerOperate(void)
+  {
+    using namespace osl;
+    // TODO cleanup
+    std::stringbuf  string_out;
+    std::streambuf* std_out = std::cout.rdbuf(&string_out);
+    NumEffectState state((SimpleState(HIRATE)));
+    bool ended = computerOperate(state);
+    std::cout << std::flush;
+    std::cout.rdbuf(std_out);
+    cut_assert_false(ended);
+  }
 
-void
-test_playerOperate_valid(void)
-{
-  // Arrange
-  using namespace osl;
-  std::stringbuf  string_out;
-  std::streambuf* std_out = std::cout.rdbuf(&string_out);
-  NumEffectState state((SimpleState(HIRATE)));
-  const char* expected = "\
+  void
+  test_playerOperate_valid(void)
+  {
+    // Arrange
+    using namespace osl;
+    std::stringbuf  string_out;
+    std::streambuf* std_out = std::cout.rdbuf(&string_out);
+    NumEffectState state((SimpleState(HIRATE)));
+    const char* expected = "\
 P1-KY-KE-GI-KI-OU-KI-GI-KE-KY\n\
 P2 * -HI *  *  *  *  * -KA * \n\
 P3-FU-FU-FU-FU-FU-FU-FU-FU-FU\n\
@@ -83,47 +83,47 @@ P9+KY+KE+GI+KI+OU+KI+GI+KE+KY\n\
 \n\
 +7776FU\n";
 
-  // Act
-  bool failed = playerOperate(state, "+7776FU");
+    // Act
+    bool failed = playerOperate(state, "+7776FU");
 
-  std::cout << std::flush;
-  std::cout.rdbuf(std_out);
+    std::cout << std::flush;
+    std::cout.rdbuf(std_out);
 
-  // TODO assert it as a std::string
-  std::string str = string_out.str();
-  const char* actual = str.c_str();
+    // TODO assert it as a std::string
+    std::string str = string_out.str();
+    const char* actual = str.c_str();
 
-  // Assert
-  cut_assert_false(failed);
-  cut_assert_equal_string(expected, actual);
-}
+    // Assert
+    cut_assert_false(failed);
+    cut_assert_equal_string(expected, actual);
+  }
 
-void
-test_playerOperate_invalid_movement(void)
-{
-  using namespace osl;
-  // TODO cleanup
-  std::stringbuf  string_out;
-  std::streambuf* std_out = std::cout.rdbuf(&string_out);
-  NumEffectState state((SimpleState(HIRATE)));
-  bool failed = playerOperate(state, "+5251FU");
-  std::cout << std::flush;
-  std::cout.rdbuf(std_out);
-  cut_assert_true(failed);
-}
+  void
+  test_playerOperate_invalid_movement(void)
+  {
+    using namespace osl;
+    // TODO cleanup
+    std::stringbuf  string_out;
+    std::streambuf* std_out = std::cout.rdbuf(&string_out);
+    NumEffectState state((SimpleState(HIRATE)));
+    bool failed = playerOperate(state, "+5251FU");
+    std::cout << std::flush;
+    std::cout.rdbuf(std_out);
+    cut_assert_true(failed);
+  }
 
-void
-test_playerOperate_invalid_format(void)
-{
-  using namespace osl;
-  // TODO cleanup
-  std::stringbuf  string_out;
-  std::streambuf* std_out = std::cout.rdbuf(&string_out);
-  NumEffectState state((SimpleState(HIRATE)));
-  bool failed = playerOperate(state, "");
-  std::cout << std::flush;
-  std::cout.rdbuf(std_out);
-  cut_assert_true(failed);
-}
+  void
+  test_playerOperate_invalid_format(void)
+  {
+    using namespace osl;
+    // TODO cleanup
+    std::stringbuf  string_out;
+    std::streambuf* std_out = std::cout.rdbuf(&string_out);
+    NumEffectState state((SimpleState(HIRATE)));
+    bool failed = playerOperate(state, "");
+    std::cout << std::flush;
+    std::cout.rdbuf(std_out);
+    cut_assert_true(failed);
+  }
 
 }  // namespace tuishogi
